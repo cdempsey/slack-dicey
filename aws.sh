9d12b1a4f2b92f9d700d8f54b652be08c916645f
Chris Dempsey <cdallas@gmail.com>
2016-07-15 16:08:29 -0500

    Excluded .DS_Store files from the AWS Lambda ZIP file

--- aws.sh
+++ aws.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir -p build
-zip -r ./build/dicey.zip ./node_modules/*
+zip -r ./build/dicey.zip ./node_modules/* -x "*.DS_Store"
 zip -r ./build/dicey.zip ./src/*.js
 zip -r ./build/dicey.zip ./*.md
